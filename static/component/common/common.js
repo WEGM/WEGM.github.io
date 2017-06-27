e7ef1955278e4ea8fe846850e3d3e36d1075549d
luoyao <luoyao@seedui.com>
2017-06-27 15:59:30 +0800

    教育

--- static/component/common/common.js
+++ static/component/common/common.js
@@ -1,17 +1,62 @@
 var simpleTpl = function( tpl ) {
-    tpl = $.isArray( tpl ) ? tpl.join( '' ) : (tpl || '');
+	tpl = $.isArray( tpl ) ? tpl.join( '' ) : (tpl || '');
 
-    return {
-        store: tpl,
-        _: function() {
-            var me = this;
-            $.each( arguments, function( index, value ) {
-                me.store += value;
-            } );
-            return this;
-        },
-        toString: function() {
-            return this.store;
-        }
-    };
+	return {
+		store: tpl,
+		_: function() {
+			var me = this;
+			$.each( arguments, function( index, value ) {
+				me.store += value;
+			} );
+			return this;
+		},
+		toString: function() {
+			return this.store;
+		}
+	};
 };
+
+$('body').append('<div id="J_GotoTop" class="elevator"><a href="" class="elevator-msg" target="_blank"><i class="icon-feedback"></i><span class="">联系我们</span></a><a href="mobile.html" target="_blank" class="elevator-app"><i class="icon-appdownload"></i><span class="">APP下载</span><div class="elevator-app-box"></div></a><a href="javascript:void(0)" class="elevator-weixin" id="js-elevator-weixin"><i class="icon-wxgzh"></i><span class="">官方微信</span><div class="elevator-weixin-box"></div></a><a href="javascript:void(0)" class="elevator-top no-goto" style="" id="backTop"><i class="icon-up2"></i><span class="">返回顶部</span></a></div>');	
+// <div id="J_GotoTop" class="elevator">
+//     <a href="/user/feedback" class="elevator-msg" target="_blank">
+//         <i class="icon-feedback"></i>
+//         <span class="">意见反馈</span>
+//     </a>
+//     <a href="/about/faq" class="elevator-faq" target="_blank">
+//         <i class="icon-ques"></i>
+//         <span class="">常见问题</span>
+//     </a>
+//     <a href="http://www.imooc.com/mobile/app" target="_blank" class="elevator-app">
+//         <i class="icon-appdownload"></i>
+//         <span class="">APP下载</span>
+//         <div class="elevator-app-box"></div>
+//     </a>
+//     <a href="javascript:void(0)" class="elevator-weixin" id="js-elevator-weixin">
+//         <i class="icon-wxgzh"></i>
+//         <span class="">官方微信</span>
+//         <div class="elevator-weixin-box"></div>
+//     </a>
+//     <a href="javascript:void(0)" class="elevator-top no-goto" style="" id="backTop">
+//         <i class="icon-up2"></i>
+//         <span class="">返回顶部</span>
+//     </a>
+// </div>
+
+function isTop() {
+	height = $(window).height(),
+	scrollTop = $(document).scrollTop(),
+	scrollTop >= 768 ? ($("#backTop").show(),
+	$("#js-elevator-weixin").removeClass("no-goto")) : ($("#backTop").hide(),
+	$("#js-elevator-weixin").addClass("no-goto"))
+};
+
+isTop();
+$(window).scroll(function() {
+	isTop();
+});
+
+$("#backTop").click(function() {
+	$("html,body").animate({
+		scrollTop: 0
+	}, 200)
+});
---
+++ static/page/lecturer/lecturer.js
@@ -0,0 +1,128 @@
+particlesJS('particles-js',
+  {
+	"particles": {
+	  "number": {
+		"value": 80,
+		"density": {
+		  "enable": true,
+		  "value_area": 800
+		}
+	  },
+	  "color": {
+		"value": "#e3f3ff"
+	  },
+	  "shape": {
+		"type": "circle",
+		"stroke": {
+		  "width": 0,
+		  "color": "#000000"
+		},
+		"polygon": {
+		  "nb_sides": 5
+		}
+	  },
+	  "opacity": {
+		"value": 0.5,
+		"random": false,
+		"anim": {
+		  "enable": false,
+		  "speed": 1,
+		  "opacity_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "size": {
+		"value": 5,
+		"random": true,
+		"anim": {
+		  "enable": false,
+		  "speed": 40,
+		  "size_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "line_linked": {
+		"enable": true,
+		"distance": 150,
+		"color": "#e3f3ff",
+		"opacity": 0.4,
+		"width": 1
+	  },
+	  "move": {
+		"enable": true,
+		"speed": 6,
+		"direction": "none",
+		"random": false,
+		"straight": false,
+		"out_mode": "out",
+		"attract": {
+		  "enable": false,
+		  "rotateX": 600,
+		  "rotateY": 1200
+		}
+	  }
+	},
+	"interactivity": {
+	  "detect_on": "canvas",
+	  "events": {
+		"onhover": {
+		  "enable": true,
+		  "mode": "repulse"
+		},
+		"onclick": {
+		  "enable": true,
+		  "mode": "push"
+		},
+		"resize": true
+	  },
+	  "modes": {
+		"grab": {
+		  "distance": 400,
+		  "line_linked": {
+			"opacity": 1
+		  }
+		},
+		"bubble": {
+		  "distance": 400,
+		  "size": 40,
+		  "duration": 2,
+		  "opacity": 8,
+		  "speed": 3
+		},
+		"repulse": {
+		  "distance": 200
+		},
+		"push": {
+		  "particles_nb": 4
+		},
+		"remove": {
+		  "particles_nb": 2
+		}
+	  }
+	},
+	"retina_detect": true,
+	"config_demo": {
+	  "hide_card": false,
+	  "background_color": "#FFF",
+	  "background_image": "",
+	  "background_position": "50% 50%",
+	  "background_repeat": "no-repeat",
+	  "background_size": "cover"
+	}
+  }
+);
+
+var teacherLists = '';
+var teacherTpl = simpleTpl();
+teacherTpl._('<div class="lists-content-card col-lg-2-4 col-md-3 col-sm-3 col-xs-6">')
+	._('<a target="_blank" class="teacher-card g-shadow" href="">')
+		._('<div class="teacher-card-cover" style="background:url(/static/images/avatar/avatar2.jpg) center center no-repeat;background-size:cover;"></div>')
+		._('<div class="teacher-card-content">')
+			._('<h3 class="teacher-card-name ui-nowrap-flex">教师姓名</h3>')
+			._('<p class="teacher-card-school ui-nowrap-flex">清华大学研究生讲师</p>')
+			._('<p class="teacher-card-detail ui-nowrap-multi-4">拥有多年带领华为数据挖掘团队给全世界范围内著名运营商提供大数据解决方案的经验所带领的团队主要负责处理、挖掘和分析每天数以TB计的数据。作为一个带领拥有20人团队成功完成多个全球项目的leader,非常了解当今用途</p>')
+		._('</div>')
+	._('</a>')
+._('</div>');
+for (var i = 0; i < 25; i++) teacherLists += teacherTpl;
+$('.teacher-lists .lists-content').html(teacherLists);
